873ae3b0e0587c7f6d180d060b62eb9e0e7aae14
Phoenix070899 <nguyentrungdung61@gmail.com>
2022-07-04 23:23:58 +0700

    Finish

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -11,30 +11,30 @@ function About() {
         </div>
       </div>
       <p>
-        Dear Human Resources, I am writing to introduce myself to you, hoping
-        that you will agree to work with me this year to develop my career
-        goals. I am interested in working in the Development field to explore
-        career opportunities and learn about career fields. Have strong
-        fundamental skills in HTML, CSS, JS, and I want to apply them in a
+        I am writing to introduce myself to you, hoping that you will give me an
+        opportunities to work with your company to develop my career goals. I am
+        interested in working in the Development field to explore career
+        opportunities and learn about career fields. Have strong fundamental
+        skills in HTML, CSS, JS, ReactJS and I want to apply them in a
         professional environment.
       </p>
       <p>
         As my passion for this field, I will improve my knowledge not only in
-        jobs but also in every challenge to extend my limitation. I am a fresher
-        Front - End Developer, and thinking of pursuing a career in it. I know
-        that I like creating functional, user-friendly websites and I have made
-        some projects while I am learning. My studies have given me an
-        understanding of how to use advanced tools to understand the process of
-        creating websites efficiently.
+        jobs but also in every challenge to extend my limitation. I am a Front -
+        End Developer, and thinking of pursuing a career in it. I know that I
+        want to create functional, user-friendly websites and I have made some
+        projects while I was learning. My studies have given me an understanding
+        of how to use advanced tools and understand the process of creating
+        websites efficiently.
       </p>
       <p>
         In the previous position, I worked as a Graphic Design on E-commerce
         platforms such as Shopee, Lazada, and Tiki at Lixibox Cosmetic. I
         believe I have gained insight into the importance of building a
         memorable consumer experience that aligns with a brand. I have also had
-        to learn how to collaborate as part of a team, communicate clearly, and
-        make decisions when necessary. I believe I could contribute to your team
-        and look forward to hearing from you.
+        to learn how to collaborate with others as part of a team, communicate
+        clearly, and make decisions when necessary. I believe I could contribute
+        to your team. I am looking forward to hearing from you.
       </p>
       <p>Sincerely, Trung Dũng</p>
     </div>
